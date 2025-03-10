d8fd5e4cfbafc58258ecc17c3d5b9bf788801bb3
ocusnir <102429445+ocusnir@users.noreply.github.com>
2025-03-11 00:55:51 +0200

    updated

--- app/src/main/java/com/example/composetestlaunch/Utils.kt
+++
@@ -1,8 +0,0 @@
-package com.example.composetestlaunch
-
-import android.icu.util.Calendar
-
-fun calculateAge(birthYear: Int): Int {
-    val currentYear = Calendar.getInstance().get(Calendar.YEAR)
-    return if (birthYear in 1900..currentYear) currentYear - birthYear else 0
-}
